26c625a7f33aadd30541f487e2142033473bb48b
Rhys Evans <mrevinz@gmail.com>
2020-04-18 18:43:23 +0100

    folder is now created@

--- MenuNotes/MenuNotes/NotesViewController.swift
+++ MenuNotes/MenuNotes/NotesViewController.swift
@@ -5,40 +5,54 @@ class NotesViewController: NSViewController {
     @IBOutlet weak var firstTextBox: NSScrollView!
     @IBOutlet weak var quitButton: NSButton!
     
+    let FOLDER_URL = URL(string: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.absoluteString + "MenuNotes/")
+    
     class func loadFromNib() -> NotesViewController{
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
         return storyboard.instantiateController(withIdentifier: "NotesViewController") as! NotesViewController
     }
     
     override func viewDidLoad() {
+        CreateDirectory()
         super.viewDidLoad()
         quitButton.action = #selector(self.quitApplication)
         firstTextBox.documentView!.insertText(ReadData(fileName: "notes.txt"))
         secondTextBox.documentView!.insertText(ReadData(fileName: "other.txt"))
     }
     
-    @objc func quitApplication()
-    {
+    @objc func quitApplication() {
         SaveData()
         NSApplication.shared.terminate(self)
     }
+    
+    func CreateDirectory() {
+        let documentsDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let docURL = URL(string: documentsDirectory)!
+        let dataPath = docURL.appendingPathComponent("MenuNotes")
+        if !FileManager.default.fileExists(atPath: dataPath.absoluteString) {
+            do {
+                try FileManager.default.createDirectory(atPath: dataPath.absoluteString, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print(error.localizedDescription);
+            }
+        }
+    }
 
     func ReadData(fileName: String) -> String {
-        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
         do{
-            return try String(contentsOf: (dir?.appendingPathComponent(fileName))!, encoding: .utf8)
+            return try String(contentsOf: (FOLDER_URL?.appendingPathComponent(fileName))!, encoding: .utf8)
         }
         catch {
             return ""
         }
     }
     
-    func SaveData() -> Void {
+    func SaveData() {
         WriteData(fileName: "notes.txt")
         WriteData(fileName: "other.txt")
     }
     
-    func WriteData(fileName: String) -> Void { // Eventually return array of data
+    func WriteData(fileName: String) { // Eventually return array of data
         var scrollView:NSScrollView = firstTextBox
         
         if fileName != "notes.txt"{
@@ -47,10 +61,9 @@ class NotesViewController: NSViewController {
         
         let myTextView: NSTextView = scrollView.documentView! as! NSTextView
         let text:String = myTextView.string
-        
-        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+
         do{
-            try text.write(to: (dir?.appendingPathComponent(fileName))!, atomically: false, encoding: .utf8)
+            try text.write(to: (FOLDER_URL?.appendingPathComponent(fileName))!, atomically: false, encoding: .utf8)
         }
         catch {
             return
